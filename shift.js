12bcc62dd6ef07f5cc8de0e4a8620c785577cc20
thirdcoder <boris3rd@bk.ru>
2015-07-14 18:49:04 -0700

    Add shift-right, shr

--- shift.js
+++ shift.js
@@ -4,6 +4,11 @@ function shl(input, carryIn) {
   return input * 3 + carryIn;
 }
 
+function shr(input) {
+  return Math.round(input / 3);
+}
+
 module.exports = {
   shl: shl,
+  shr: shr,
 };
--- test.js
+++ test.js
@@ -3,6 +3,7 @@
 var test = require('tape');
 var bts2n = require('balanced-ternary').bts2n;
 var shl = require('./').shl;
+var shr = require('./').shr;
 
   test('shift left zero', function(t) {
   t.equal(shl(0, 0), 0);
@@ -29,6 +30,18 @@ test('shift left repeated', function(t) {
   t.equal(shl(shl(shl(bts2n('10i'),-1),-1),-1), bts2n('10iiii'));
 
   t.equal(shl(shl(shl(bts2n('10i'),-1), 0), 1), bts2n('10ii01'));
+
+  t.end();
+});
+
+test('shift right', function(t) {
+  t.equal(shr(bts2n('10')), bts2n('1'));
+  t.equal(shr(bts2n('100')), bts2n('10'));
+  t.equal(shr(bts2n('i0')), bts2n('i'));
+  t.equal(shr(bts2n('i00')), bts2n('i0'));
+
+  t.equal(shr(bts2n('101')), bts2n('10'));
+  t.equal(shr(bts2n('i0i')), bts2n('i0'));
 
   t.end();
 });
