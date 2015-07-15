bc30d5f47ab3d22d49b136a2f81c747d6b7b2145
thirdcoder <boris3rd@bk.ru>
2015-07-14 18:39:55 -0700

    Optional shift carry in

--- test.js
+++ test.js
@@ -4,16 +4,18 @@ var test = require('tape');
 var bts2n = require('balanced-ternary').bts2n;
 var shift_left = require('./').shift_left;
 
-test('shift left carry zero', function(t) {
-  t.equal(shift_left(0, 0), 0);
-  t.equal(shift_left(1, 0), bts2n('10'));
-  t.equal(shift_left(-1, 0), bts2n('i0'));
-  t.equal(shift_left(bts2n('111iii'), 0), bts2n('111iii0'));
+test('shift left', function(t) {
+  t.equal(shift_left(0), 0);
+  t.equal(shift_left(1), bts2n('10'));
+  t.equal(shift_left(-1), bts2n('i0'));
+  t.equal(shift_left(bts2n('111iii')), bts2n('111iii0'));
   t.end();
 });
 
 test('shift left carry', function(t) {
+  t.equal(shift_left(bts2n('i01'), 0), bts2n('i010'));
   t.equal(shift_left(bts2n('100'), -1), bts2n('100i'));
   t.equal(shift_left(bts2n('111iii'), 1), bts2n('111iii1'));
   t.end();
 });
+
