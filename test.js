8a17bedf2a497164745effb91fd36947fe7850db
thirdcoder <boris3rd@bk.ru>
2015-07-20 21:48:24 -0700

    Add more tests

--- test.js
+++ test.js
@@ -9,6 +9,16 @@ test('least-significant trit', function(t) {
   t.equal(lst(1), 1);
   t.equal(lst(-1), -1);
 
+  t.equal(lst(2), -1);
+  t.equal(lst(3), 0);
+  t.equal(lst(4), 1);
+  t.equal(lst(5), -1);
+
+  t.equal(lst(-2), 1);
+  t.equal(lst(-3), 0);
+  t.equal(lst(-4), -1);
+  t.equal(lst(-5), 1);
+
   t.equal(lst(bts2n('10')), 0);
   t.equal(lst(bts2n('1i')), -1);
   t.equal(lst(bts2n('11')), 1);
@@ -20,6 +30,9 @@ test('least-significant trit', function(t) {
   t.equal(lst(bts2n('101i10i10i1i')), -1);
   t.equal(lst(bts2n('i10i00000i10')), 0);
   t.equal(lst(bts2n('010i0iiii011')), 1);
+
+  t.throws(function() { lst(NaN); });
+
   t.end();
 });
 
